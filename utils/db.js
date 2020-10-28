4354db5c45464d2d3c3d30538d2c5c98f7cc5318
Onkar Thorat <onkarthorat2404@gmail.com>
2020-10-28 09:22:13 +0530

    changes to db.js

--- utils/db.js
+++ utils/db.js
@@ -22,12 +22,12 @@ function setupDb() {
     // TODO Add all table create queries here
 
 
-    db.query('CREATE TABLE IF NOT EXISTS user(userID INT NOT NULL PRIMARY KEY, name VARCHAR(20), \
+    db.query('CREATE TABLE IF NOT EXISTS user(userID INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(20), \
       mobile VARCHAR(20))', (err, results, fields) => {
       console.log(results); // results contains rows returned by server
     });
 
-    db.query('CREATE TABLE IF NOT EXISTS theatre(theatreID INT NOT NULL PRIMARY KEY, \
+    db.query('CREATE TABLE IF NOT EXISTS theatre(theatreID INT NOT NULL AUTO_INCREMENT PRIMARY KEY, \
       name VARCHAR(20),location VARCHAR(20), phone VARCHAR(20), no_of_screens INT)', (err, results, fields) => {
       console.log(results); // results contains rows returned by server
     });
@@ -37,7 +37,7 @@ function setupDb() {
       console.log(results); // results contains rows returned by server
     });
 
-    db.query('CREATE TABLE IF NOT EXISTS event(eventID INT NOT NULL PRIMARY KEY, duration VARCHAR(20),\
+    db.query('CREATE TABLE IF NOT EXISTS event(eventID INT NOT NULL AUTO_INCREMENT PRIMARY KEY, duration VARCHAR(20),\
               lang VARCHAR(20), genre VARCHAR(20), name VARCHAR(20))', (err, results, fields) => {
       console.log(results); // results contains rows returned by server
     });
@@ -57,7 +57,7 @@ function setupDb() {
       console.log(results); // results contains rows returned by server
     });
 
-    db.query('CREATE TABLE IF NOT EXISTS screen(screenID INT NOT NULL PRIMARY KEY, capacity INT, theatreID INT,\
+    db.query('CREATE TABLE IF NOT EXISTS screen(screenID INT NOT NULL AUTO_INCREMENT PRIMARY KEY, capacity INT, theatreID INT,\
                     CONSTRAINT fk_theatre FOREIGN KEY (theatreID) REFERENCES theatre(theatreID))', (err, results, fields) => {
       console.log(results); // results contains rows returned by server
     });
@@ -67,13 +67,13 @@ function setupDb() {
       console.log(results); // results contains rows returned by server
     });
 
-    db.query('CREATE TABLE IF NOT EXISTS shows(showID INT NOT NULL PRIMARY KEY, time datetime, date date, \
+    db.query('CREATE TABLE IF NOT EXISTS shows(showID INT NOT NULL AUTO_INCREMENT PRIMARY KEY, time datetime, date date, \
       seats_avail INT, eventID INT, screenID INT, CONSTRAINT fk3_event FOREIGN KEY (eventID) REFERENCES event(eventID), \
       CONSTRAINT fk1_screen FOREIGN KEY (screenID) REFERENCES screen(screenID))', (err, results, fields) => {
       console.log(results); // results contains rows returned by server
     });
 
-    db.query('CREATE TABLE IF NOT EXISTS ticket(ticketID INT NOT NULL PRIMARY KEY, no_seats INT, userID INT, showID INT,\
+    db.query('CREATE TABLE IF NOT EXISTS ticket(ticketID INT NOT NULL AUTO_INCREMENT PRIMARY KEY, no_seats INT, userID INT, showID INT,\
       CONSTRAINT fk1_user FOREIGN KEY (userID) REFERENCES user(userID), \
       CONSTRAINT fk_show FOREIGN KEY (showID) REFERENCES shows(showID))', (err, results, fields) => {
       console.log(results); // results contains rows returned by server
