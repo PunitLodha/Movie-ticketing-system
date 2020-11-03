f4802228d024ed183760be19ab0c0eb03b965dd4
PunitLodha <punitlodha@pm.me>
2020-11-03 13:22:02 +0530

    added routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,21 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
+import Register from './components/Register/Register';
+import Login from './components/Login/Login';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Navbar />
-      <Home />
-    </div>
+      <Switch>
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </Router>
   );
 }
 
